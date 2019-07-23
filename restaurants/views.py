23e5f5073a25ad65da18fe00fe835fcbad134092
RozeTarig <rose@MacBook-Pro-Rose.local>
2019-07-23 19:16:26 +0300

    solution 03

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,12 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list" :[
+    {"restaurant_name":"restaurant_A" , "food_type":"food_A"},
+    {"restaurant_name":"restaurant_B" , "food_type":"food_B"},
+    {"restaurant_name":"restaurant_C" , "food_type":"food_C"},
+
+    ],
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +20,8 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object":
+    {"restaurant_name":"restaurant_R" , "food_type":"food_R"},
 
     }
     return render(request, 'detail.html', context)
